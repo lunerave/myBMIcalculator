d07e7d7c274b6657adcdbd501f442cb03e6fe999
이상록 <lune@pc-10-32.customer.ask4.lan>
2022-10-12 20:19:09 +0200

    build back button to go back first page and modify BMI calculation as round to the first decimal place.

--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -2,8 +2,10 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import kotlin.math.pow
+import kotlin.math.round
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +15,10 @@ class ResultActivity : AppCompatActivity() {
         val height = intent.getIntExtra("Height", 0)
         val weight = intent.getIntExtra("Weight", 0)
 
-        val bmi = weight / (height / 100.0).pow(2.0)
+        val bmi = round((weight / (height / 100.0).pow(2.0))*10)/10
         val resultText = when{
-            bmi >= 23.0 -> "Overweight"
-            bmi <= 18.5 -> "Normal"
+            bmi >= 23 -> "Overweight"
+            bmi >= 18.5 -> "Normal"
             else -> "Underweight"
         }
         val resultValueTextView = findViewById<TextView>(R.id.txt_bmi)
@@ -24,5 +26,11 @@ class ResultActivity : AppCompatActivity() {
 
         resultValueTextView.text = bmi.toString()
         resultStringTextView.text = resultText
+
+        val closeButton = findViewById<Button>(R.id.buttonClose)
+
+        closeButton.setOnClickListener {
+            finish()
+        }
     }
 }
