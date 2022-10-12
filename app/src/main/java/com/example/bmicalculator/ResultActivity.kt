c1be30370562bd24fff7ee8b883f93214ed96767
이상록 <lune@pc-10-32.customer.ask4.lan>
2022-10-12 23:07:05 +0200

    Add emoji image in the result page and modify color of the result text

--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -3,6 +3,7 @@ package com.example.bmicalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.math.pow
 import kotlin.math.round
@@ -14,12 +15,25 @@ class ResultActivity : AppCompatActivity() {
 
         val height = intent.getIntExtra("Height", 0)
         val weight = intent.getIntExtra("Weight", 0)
+        val imageView = findViewById<ImageView>(R.id.imageView)
 
         val bmi = round((weight / (height / 100.0).pow(2.0))*10)/10
         val resultText = when{
-            bmi >= 23 -> "Overweight"
+            bmi >= 23 ->
+                "Overweight"
+
             bmi >= 18.5 -> "Normal"
             else -> "Underweight"
+        }
+
+        when {
+            bmi >= 23 ->
+                imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            bmi >= 18.5 ->
+                imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_satisfied_24)
+            else ->
+                imageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+
         }
         val resultValueTextView = findViewById<TextView>(R.id.txt_bmi)
         val resultStringTextView = findViewById<TextView>(R.id.txt_result)
